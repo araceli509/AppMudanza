0e5d192fc53d51b319382c9d4b0b576582dda679
Angelo-12 <angel23.aj32@gmail.com>
2019-10-31 21:28:43 -0600

    Login del prestador de servicio,creacion del fragment prestador servicio y creacion de la activity prestador servicio, diseño del login

--- app/src/main/java/com/example/appmudanzas/MainActivity.java
+++ app/src/main/java/com/example/appmudanzas/MainActivity.java
@@ -2,22 +2,31 @@ package com.example.appmudanzas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 //araceli
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn_prestador_servicio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_prestador_servicio=findViewById(R.id.btn_prestador_servicio);
+        btn_prestador_servicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(getApplicationContext(),Prestador_Servicio_Activity.class);
+                startActivity(i);
+            }
+        });
         primercommitjulian();
     }
 
     protected void hola(){
-
-
 
     }
     protected void numero(){
---
+++ app/src/main/java/com/example/appmudanzas/Prestador_Servicio_Activity.java
@@ -0,0 +1,23 @@
+package com.example.appmudanzas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+public class Prestador_Servicio_Activity extends AppCompatActivity implements Login_Prestador_Servicio_Fragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prestador__servicio_);
+
+        Login_Prestador_Servicio_Fragment fragment1= new Login_Prestador_Servicio_Fragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.contenedor,fragment1).commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
