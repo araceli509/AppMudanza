f29818fb75cf0a5b102bf71498d939ad1656c366
Angelo-12 <angel23.aj32@gmail.com>
2019-11-02 10:42:47 -0600

    Asignando permisos de escritura y lectura para acceder a la camara del dispositivo

--- app/src/main/java/com/example/appmudanzas/Registro_Foto_Perfil_Fragment.java
+++ app/src/main/java/com/example/appmudanzas/Registro_Foto_Perfil_Fragment.java
@@ -1,17 +1,40 @@
 package com.example.appmudanzas;
 
+import android.Manifest;
+import android.Manifest.permission;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.net.URI;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,17 +49,25 @@ public class Registro_Foto_Perfil_Fragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
+    private static final String CARPETA_PRINCIPAL="misImagenesApp/";
+    private static final String CARPETA_IMAGEN="imaganes";
+    private static final String DIRECTORIO_IMAGEN=CARPETA_PRINCIPAL+CARPETA_IMAGEN;
+    private String path;
+    private File fileImagen;
+    private Bitmap bitmap;
+    private static final int COD_SELECCIONA =10 ;
+    private static final int COD_FOTO=20;
     private String mParam1;
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
     private View vista;
     private Button btn_registrar_foto_perfil;
+    private Button btnFoto;
+    private ImageView imagePerfil;
 
     public Registro_Foto_Perfil_Fragment() {
-        // Required empty public constructor
+
     }
 
     /**
@@ -70,6 +101,24 @@ public class Registro_Foto_Perfil_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         vista=inflater.inflate(R.layout.fragment_registro__foto__perfil_, container, false);
+
+        btnFoto=vista.findViewById(R.id.btnFoto);
+        imagePerfil=vista.findViewById(R.id.imagePerfil);
+
+        if(validaPermisos()){
+            btnFoto.setEnabled(true);
+        }else{
+            btnFoto.setEnabled(false);
+        }
+
+        btnFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarDialogOpciones();
+            }
+        });
+
+
         btn_registrar_foto_perfil=vista.findViewById(R.id.btn_registrar_foto_perfil);
         final Registro_Ine_Fragment registro_ine_fragment= new Registro_Ine_Fragment();
 
@@ -85,7 +134,94 @@ public class Registro_Foto_Perfil_Fragment extends Fragment {
         return vista;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+    private boolean validaPermisos() {
+        return true;
+    }
+
+    private void mostrarDialogOpciones() {
+        final CharSequence []opciones={"Tomar Foto","Elegir de Galeria","Cancelar"};
+        final AlertDialog.Builder builder= new AlertDialog.Builder(getContext());
+        builder.setTitle("Elige una opcion");
+        builder.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                if(opciones[i].equals("Tomar Foto")){
+                    abrirCamara();
+                    Toast.makeText(getContext(),"Tomando foto del dispositivo",Toast.LENGTH_SHORT).show();
+                }else {
+                    if(opciones[i].equals("Elegir de Galeria")){
+                        Intent intent= new Intent(Intent.ACTION_PICK,
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        intent.setType("image/");
+                        startActivityForResult(intent.createChooser(intent,"Seleccione"),COD_SELECCIONA);
+                    }else{
+                        dialog.dismiss();
+                    }
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private void abrirCamara() {
+        File miFile=new File(Environment.getExternalStorageDirectory(),DIRECTORIO_IMAGEN);
+        boolean isCreada=miFile.exists();
+
+        if(isCreada==false){
+            isCreada=miFile.mkdirs();
+        }
+
+        if(isCreada){
+            Long consecutivo=System.currentTimeMillis()/1000;
+            String nombre=consecutivo.toString()+".jpg";
+            path=Environment.getExternalStorageDirectory()+File.separator+DIRECTORIO_IMAGEN+
+                    File.separator+nombre;
+            fileImagen= new File(path);
+
+            Intent intent=null;
+            intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.N){
+                String authorities=getContext().getPackageName()+".provider";
+                Uri imageUri=FileProvider.getUriForFile(getContext(),authorities,fileImagen);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+            }else{
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(fileImagen));
+            }
+            startActivityForResult(intent,COD_FOTO);;
+        }
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode==RESULT_OK){
+
+        }
+
+        switch (requestCode){
+            case COD_SELECCIONA:
+                Uri miPath=data.getData();
+                imagePerfil.setImageURI(miPath);
+                break;
+
+            case COD_FOTO:
+                MediaScannerConnection.scanFile(getContext(), new String[]{path}, null,
+                        new MediaScannerConnection.OnScanCompletedListener() {
+                            @Override
+                            public void onScanCompleted(String path, Uri uri) {
+                                Log.i("Path",path);
+                            }
+                        });
+                bitmap= BitmapFactory.decodeFile(path);
+                imagePerfil.setImageBitmap(bitmap);
+                break;
+        }
+    }
+
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
