f839deaeee3fa64478bdf8c8a8ed5001168fbb2a
Diana Rojas <dianitarojas27@gmail.com>
2019-11-18 11:49:43 -0600

    Primer commit diana rojas arreola jejej

---
+++ app/src/main/java/com/example/appmudanzas/Login/LoginPrincipal.java
@@ -0,0 +1,16 @@
+package com.example.appmudanzas.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appmudanzas.R;
+
+public class LoginPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_principal);
+    }
+}
