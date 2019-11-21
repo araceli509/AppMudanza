9b22501b0e80c1f03b264917ffaf459716cda6e9
JulianRamirezLuisGerardo <luisgerardojulianramirez2912@gmail.com>
2019-11-21 14:10:42 -0600

    modificacion

--- app/src/main/java/com/example/appmudanzas/RecyclerView/InicioFragment.java
+++ app/src/main/java/com/example/appmudanzas/RecyclerView/InicioFragment.java
@@ -4,16 +4,13 @@ package com.example.appmudanzas.RecyclerView;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -23,18 +20,10 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.appmudanzas.Cotizacion.Cotizacion;
 import com.example.appmudanzas.R;
-import com.example.appmudanzas.prestador_Servicio.solicitud_preview;
-import com.example.appmudanzas.prestador_Servicio.solicitudes.cliente;
-import com.example.appmudanzas.prestador_Servicio.solicitudes.reservacion;
-import com.example.appmudanzas.prestador_Servicio.solicitudes.solicitudAdapter;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -42,9 +31,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.sql.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -54,7 +40,6 @@ import java.util.List;
  * A simple {@link Fragment} subclass.
  */
 public class InicioFragment extends Fragment implements Response.Listener<JSONObject>, Response.ErrorListener {
-    private InicioFragment.OnFragmentInteractionListener mListener;
     RecyclerView recyclerView;
     List<ChoferPojo> choferes;
     ChoferAdapter adapter;
@@ -67,7 +52,6 @@ public class InicioFragment extends Fragment implements Response.Listener<JSONOb
     private JsonObjectRequest jsonObjectRequest;
 
     public InicioFragment() {
-        // Required empty public constructor
     }
 
     @Override
@@ -94,12 +78,7 @@ public class InicioFragment extends Fragment implements Response.Listener<JSONOb
         recyclerView.setAdapter(adapter);
         return v;
     }
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+
     public static boolean compruebaConexion(Context context) {
         boolean connected = false;
         ConnectivityManager connec = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -117,7 +96,6 @@ public class InicioFragment extends Fragment implements Response.Listener<JSONOb
     public void obtenerDatos() {
         boolean conexion=compruebaConexion(getContext());
         if(conexion) {
-            Toast.makeText(getContext(),obtenerHora(),Toast.LENGTH_LONG).show();
             String url = "http://mudanzito.site/api/auth/prestador_servicio/horario_tarifa/" + obtenerHora();
             jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, this, this);
             request.add(jsonObjectRequest);
@@ -163,24 +141,4 @@ public class InicioFragment extends Fragment implements Response.Listener<JSONOb
         return dato;
     }
 
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof InicioFragment.OnFragmentInteractionListener) {
-            mListener = (InicioFragment.OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
