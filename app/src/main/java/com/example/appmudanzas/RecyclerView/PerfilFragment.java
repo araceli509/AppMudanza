464b00bf974ed68f47b2882cce7377cc181ea079
Luis Julian <luisgerardojulianramirez2912@gmail.com>
2019-12-15 22:31:34 -0600

    prueba

--- app/src/main/java/com/example/appmudanzas/RecyclerView/PerfilFragment.java
+++ app/src/main/java/com/example/appmudanzas/RecyclerView/PerfilFragment.java
@@ -1,34 +1,51 @@
 package com.example.appmudanzas.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.appmudanzas.Cotizacion.Servicio_ExtraPojo;
 import com.example.appmudanzas.R;
+import com.example.appmudanzas.prestador_Servicio.VolleySingleton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PerfilFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PerfilFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class PerfilFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class PerfilFragment extends Fragment implements Response.ErrorListener,Response.Listener<JSONObject> {
+    private EditText txtnombre;
+    private EditText txtapellidos;
+    private EditText txtcorreo;
+    private EditText txtdireccion;
+    private EditText txttelefono;
+    private EditText txtcodigo_postal;
+    private EditText txtfecha_registro;
+    private ProgressDialog progreso2;
+    private FirebaseAuth mAuth;
+    RequestQueue request;
+    private JsonObjectRequest jsonObjectRequest;
 
     private OnFragmentInteractionListener mListener;
 
@@ -36,30 +53,11 @@ public class PerfilFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PerfilFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PerfilFragment newInstance(String param1, String param2) {
-        PerfilFragment fragment = new PerfilFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -67,7 +65,18 @@ public class PerfilFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        View vista = inflater.inflate(R.layout.fragment_perfil, container, false);
+        txtnombre = vista.findViewById(R.id.txtNombre);
+        txtapellidos = vista.findViewById(R.id.txtApellidos);
+        txtcorreo = vista.findViewById(R.id.txtcorreo);
+        txtdireccion = vista.findViewById(R.id.txtdireccion);
+        txttelefono = vista.findViewById(R.id.txttelefono);
+        txtcodigo_postal = vista.findViewById(R.id.txtcodigo_postal);
+        txtfecha_registro = vista.findViewById(R.id.txtfecha_registro);
+        request = Volley.newRequestQueue(getContext());
+        mAuth = FirebaseAuth.getInstance();
+        obtenerPerfilUsuario();
+        return vista;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -94,18 +103,75 @@ public class PerfilFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+    }
+
+    private void getDatos() {
+
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        Gson gson = new GsonBuilder().create();
+        PerfilPojo choferpojo=null;
+        try {
+            JSONArray json = response.getJSONArray("servicios_extras");
+            for(int i = 0; i<json.length(); i++ ) {
+                String chofer = json.getString(i);
+                choferpojo = gson.fromJson(chofer,PerfilPojo.class);
+            }
+            txtnombre.setText(choferpojo.getNombre()+"");
+            txtapellidos.setText(choferpojo.getApellidos()+"");
+            txtcorreo.setText(choferpojo.getCorreo()+"");
+            txtdireccion.setText(choferpojo.getDireccion()+"");
+            txttelefono.setText(choferpojo.getTelefono()+"");
+            txtcodigo_postal.setText(choferpojo.getCodigo_postal()+"");
+            txtfecha_registro.setText(choferpojo.getFecha_registro()+"");
+            Log.e("hola",choferpojo.getNombre());
+            Log.e("hola",choferpojo.getApellidos());
+            Log.e("hola",choferpojo.getCorreo());
+            Log.e("hola",choferpojo.getDireccion());
+            Log.e("hola",choferpojo.getTelefono());
+            Log.e("hola",choferpojo.getCodigo_postal());
+            Log.e("hola",choferpojo.getFecha_registro());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    public static boolean compruebaConexion(Context context) {
+        boolean connected = false;
+        ConnectivityManager connec = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        // Recupera todas las redes (tanto móviles como wifi)
+        NetworkInfo[] redes = connec.getAllNetworkInfo();
+
+        for (int i = 0; i < redes.length; i++) {
+            if (redes[i].getState() == NetworkInfo.State.CONNECTED) {
+                connected = true;
+            }
+        }
+        return connected;
+    }
+
+    public void obtenerPerfilUsuario() {
+        progreso2= new ProgressDialog(getContext());
+        progreso2.setMessage("Enviando");
+        progreso2.show();
+        if(compruebaConexion(getContext())){
+            String url = "http://mudanzito.site/api/auth/cliente/cliente_correo/" + mAuth.getCurrentUser().getEmail();
+            jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, (Response.Listener<JSONObject>) this, this);
+            VolleySingleton.getInstanciaVolley(getContext()).addToRequestQueue(jsonObjectRequest);
+        }else{
+            Toast.makeText(getContext(),"Revise su conexion a internet",Toast.LENGTH_LONG).show();
+        }
+        progreso2.dismiss();
     }
 }
---
+++ app/src/main/java/com/example/appmudanzas/RecyclerView/PerfilPojo.java
@@ -0,0 +1,80 @@
+package com.example.appmudanzas.RecyclerView;
+
+public class PerfilPojo {
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String direccion;
+    private String telefono;
+    private String codigo_postal;
+    private String fecha_registro;
+
+    public PerfilPojo() {
+    }
+
+    public PerfilPojo(String nombre, String apellidos, String correo, String direccion, String telefono, String codigo_postal, String fecha_registro) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.codigo_postal = codigo_postal;
+        this.fecha_registro = fecha_registro;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCodigo_postal() {
+        return codigo_postal;
+    }
+
+    public void setCodigo_postal(String codigo_postal) {
+        this.codigo_postal = codigo_postal;
+    }
+
+    public String getFecha_registro() {
+        return fecha_registro;
+    }
+
+    public void setFecha_registro(String fecha_registro) {
+        this.fecha_registro = fecha_registro;
+    }
+}
